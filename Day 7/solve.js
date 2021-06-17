03671869373c1c9b718c9502efe023ae04a8f1ee
Viniket <sviniket1201@gmail.com>
2021-06-17 16:26:18 +0530

    Day 7 updated

--- Day 7/solve.js
+++ Day 7/solve.js
@@ -54,7 +54,7 @@ var student = {
       //-----------------------------Task 6----------------------------------
       var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }];
 
-        library.sort((a,b)=>a.libraryID.localeCompare(b.libraryID))
+        // library.sort((a,b)=>a.libraryID.localeCompare(b.libraryID))
         library.sort((a,b)=>b.libraryID-a.libraryID)
         console.log(library);
       
