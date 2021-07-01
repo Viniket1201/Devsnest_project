d15bef3c675be77c7ab3a11599f9f6c9aa3aebbb
Viniket <sviniket1201@gmail.com>
2021-07-01 13:20:16 +0530

    Day15 completed

---
+++ Day15/firstapp/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <h1><button>Click me</button>Hello World!!</h1>,
+  document.getElementById('root')
+  // function getStyle() {
+  //   var btn = document.getElementsByTagName(button);
+  //   btn.style.backgroundColor = "blue";
+  // }
+  
+);
+// getStyle();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
