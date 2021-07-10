a714b812b00590d9e1bc6a401040f6c87fd4a137
Viniket <sviniket1201@gmail.com>
2021-07-10 08:30:54 +0530

    Day19 completed

---
+++ Day16/firstcard/src/chess.js
@@ -0,0 +1,54 @@
+// import './App.css';
+
+
+
+// function chess() {
+//     return (
+//         <div className="main">
+            
+//             <div className="first"></div>
+//             <div className="first"></div>
+//             <div className="first"></div>
+//             <div className="first"></div>
+//             <div className="first"></div>
+//             <div className="first"></div>
+
+//             <div className="second"></div>
+//             <div className="second"></div>
+//             <div className="second"></div>
+//             <div className="second"></div>
+//             <div className="second"></div>
+//             <div className="second"></div>
+
+//             <div className="third"></div>
+//             <div className="third"></div>
+//             <div className="third"></div>
+//             <div className="third"></div>
+//             <div className="third"></div>
+//             <div className="third"></div>
+
+//             <div className="fourth"></div>
+//             <div className="fourth"></div>
+//             <div className="fourth"></div>
+//             <div className="fourth"></div>
+//             <div className="fourth"></div>
+//             <div className="fourth"></div>
+
+//             <div className="fifth"></div>
+//             <div className="fifth"></div>
+//             <div className="fifth"></div>
+//             <div className="fifth"></div>
+//             <div className="fifth"></div>
+//             <div className="fifth"></div>
+
+//             <div className="sixth"></div>
+//             <div className="sixth"></div>
+//             <div className="sixth"></div>
+//             <div className="sixth"></div>
+//             <div className="sixth"></div>
+//             <div className="sixth"></div>
+//         </div>
+//     );
+// }
+
+// export default chess;
