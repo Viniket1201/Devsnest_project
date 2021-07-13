4b7decd920be8c80ee01993b672b60ca04a37d4e
Viniket <sviniket1201@gmail.com>
2021-07-13 14:34:13 +0530

    Day21

---
+++ Day21/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Heading from "./components/heading";
+import Clrbox from "./components/clrbox";
+
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <Heading />
+        <Clrbox />
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Day21/src/components/heading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/heading.css';
+
+function Heading(){
+    return(
+        <div className="header">
+            <h1>Callorie App</h1>
+        </div>
+    )
+}
+export default Heading;
